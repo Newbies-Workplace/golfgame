20a1ebe965fce92e837458fe702b47668f9e7a14
Kamil Wąsowski <bazixx123@gmail.com>
2022-10-18 19:28:52 +0200

    Show card details

--- Assets/Scripts/Card/CardDisplay.cs
+++ Assets/Scripts/Card/CardDisplay.cs
@@ -1,16 +1,30 @@
+using System;
+using System.Collections;
 using TMPro;
+using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UIElements;
 
-public class CardDisplay : MonoBehaviour
+public class CardDisplay : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public Card card;
     public TMP_Text name;
     public TMP_Text energyCost;
-
+    
     public void Start()
     {
         name.text = card.name;
         energyCost.text = card.energyCost.ToString();
     }
 
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        gameObject.transform.Translate(new Vector2(0, 100));
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        gameObject.transform.Translate(new Vector2(0, -100));
+    }
 }
--- Assets/Scripts/Card/CardGenerator.cs
+++ Assets/Scripts/Card/CardGenerator.cs
@@ -1,22 +1,28 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class CardGenerator : MonoBehaviour
 {
-    // todo podłaczyc do battle system
     public List<GameObject> cardPrefabs;
     public int numberOfCards = 5;
 
     private void Start()
     {
+        StartCoroutine(GenerateStarterDeck());
+    }
+
+    private IEnumerator GenerateStarterDeck()
+    {
+        yield return new WaitForSeconds(.5f);
         for (var i = 0; i < numberOfCards; i++)
-        {
+        {            
             Instantiate(
                 cardPrefabs[Random.Range(0, cardPrefabs.Count)]
             ).transform.SetParent(
                 GameObject.Find("CardList").transform, false
             );
+            yield return new WaitForSeconds(.5f);
         }
     }
 }
