e777d294c332636a75e21b82bba46640d4ecfdaf
Kamil Wąsowski <bazixx123@gmail.com>
2022-10-18 18:29:31 +0200

    Generate cards bottom of screen

--- Assets/Scripts/Card/CardGenerator.cs
+++ Assets/Scripts/Card/CardGenerator.cs
@@ -1,22 +1,22 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CardGenerator : MonoBehaviour
 {
-    //todo dodać gerenowanie na dole ekranu
     // todo podłaczyc do battle system
     public List<GameObject> cardPrefabs;
-    public int numberOfCards = 10;
+    public int numberOfCards = 5;
 
     private void Start()
     {
         for (var i = 0; i < numberOfCards; i++)
         {
             Instantiate(
-                cardPrefabs[Random.Range(0, cardPrefabs.Count)], 
-                new Vector3(Screen.width  - 150 , Screen.height - 250 - (i + 1) * 35),
-                Quaternion.identity
-            ).transform.SetParent(GameObject.Find("Canvas").transform);
+                cardPrefabs[Random.Range(0, cardPrefabs.Count)]
+            ).transform.SetParent(
+                GameObject.Find("CardList").transform, false
+            );
         }
     }
 }
