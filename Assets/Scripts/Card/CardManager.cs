36e2330e721531da8f1d916aaca1ff1d7c3924ca
Kamil Wąsowski <bazixx123@gmail.com>
2022-10-19 16:55:28 +0200

    refactor

--- Assets/Scripts/Card/CardManager.cs
+++ Assets/Scripts/Card/CardManager.cs
@@ -4,19 +4,16 @@ using UnityEngine;
 
 public class CardManager : MonoBehaviour
 {
+    public Transform cardList;
     public List<GameObject> cardPrefabs;
     public int numberOfCards = 5;
-    
+
     public IEnumerator GenerateStarterDeck()
     {
         yield return new WaitForSeconds(.5f);
         for (var i = 0; i < numberOfCards; i++)
-        {            
-            Instantiate(
-                cardPrefabs[Random.Range(0, cardPrefabs.Count)]
-            ).transform.SetParent(
-                GameObject.Find("CardList").transform, false
-            );
+        {  
+            Instantiate(cardPrefabs[Random.Range(0, cardPrefabs.Count)], cardList.transform, false);
             yield return new WaitForSeconds(.5f);
         }
     }
